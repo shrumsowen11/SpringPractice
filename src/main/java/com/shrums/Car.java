f446fea87f74e11b31dd1b17a913e6ed0e6f56cf
bhask <shrumsowen11@gmail.com>
2020-05-27 15:03:23 -0400

    UpdateTesting

--- src/main/java/com/shrums/Car.java
+++ src/main/java/com/shrums/Car.java
@@ -1,31 +1,28 @@
 package com.shrums;
 
+
+
+// TestingMyGitHubUpdating 
+
+
+
+
 public class Car {
-	
+
 	private String name;
 	private String color;
 	private String make;
 	private double price;
 	private FuelType fuelType;
-	
+
 	public void run() {
-		
-		System.out.println( name + " runs...");
+
+		System.out.println(name + " runs...");
 	}
-	
-	
-	
-	
-	  public Car() {
-		  super(); 
-	  }
-	  
-	  
-	  
-	
-	 
 
-
+	public Car() {
+		super();
+	}
 
 	public Car(String name, String color, String make, double price, FuelType fuelType) {
 		super();
@@ -36,56 +33,50 @@ public class Car {
 		this.fuelType = fuelType;
 	}
 
-
-
-
 	public FuelType getFuelType() {
 		return fuelType;
 	}
-
-
-
 
 	public void setFuelType(FuelType fuelType) {
 		this.fuelType = fuelType;
 	}
 
-
-
-
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
 	public void setColor(String color) {
 		this.color = color;
 	}
+
 	public String getMake() {
 		return make;
 	}
+
 	public void setMake(String make) {
 		this.make = make;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-
-
-
 
 	@Override
 	public String toString() {
 		return "Car [name=" + name + ", color=" + color + ", make=" + make + ", price=" + price + ", fuelType="
 				+ fuelType + "]";
 	}
-	
-	
+
 }
